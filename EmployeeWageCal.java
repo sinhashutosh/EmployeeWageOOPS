1722ee36516c0a32c3cfd9310ddd7c8b5707997d
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-22 13:37:36 +0530

    Calculating part time feature also

--- EmployeeWageCal.java
+++ EmployeeWageCal.java
@@ -3,14 +3,21 @@ package org.example;
 public class EmployeeWageCal {
     int wage_per_hour = 20;
     int full_day_hour = 8;
+    int part_time_hour = 3;
+    int wage = 0;
 
     void wageCalculator(int isPresent) {
-        int wage = 0;
-        if (isPresent == 1) {
-            wage = wage_per_hour * full_day_hour;
-            System.out.println("Employee Wage = " + wage);
-        } else {
-            System.out.println("Employee Wage = " + wage);
+        switch (isPresent) {
+            case 0:
+                wage = wage_per_hour * isPresent;
+                break;
+            case 1:
+                wage = wage_per_hour * part_time_hour;
+                break;
+            case 2:
+                wage = wage_per_hour * full_day_hour;
+                break;
         }
+        System.out.println("Employee Wage = " + wage);
     }
 }
