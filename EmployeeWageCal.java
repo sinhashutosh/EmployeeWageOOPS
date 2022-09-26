e91010cdcc9d9c3e538001369c29abd310ac4661
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-26 22:31:58 +0530

    code updated With UC8

--- EmployeeWageCal.java
+++ EmployeeWageCal.java
@@ -4,25 +4,32 @@ import java.util.Random;
 
 public class EmployeeWageCal {
     void company(String name, int wage_per_hour, int wage_per_day, int max_days, int max_hour) {
-
         Random rd = new Random();
-       /* int wage_per_hour = 20;
-        int wage_per_day = 160;*/
-
-
-        int total_wage = 0;
+//        int wage_per_hour = 20;
+//        int wage_per_day = 160;
+        int part_time_hour = 4;
         int month_days = 0;
         int hour = 0;
+        int total_wage = 0;
+
         while (month_days < 20 && hour < 100) {
             int attendance = rd.nextInt(2);
             if (attendance == 1) {
                 int time = rd.nextInt(2);
+                if (hour == 96) {
+                    System.out.println("Now You can do only Part time work");
+                    hour += part_time_hour;
+                    total_wage = hour * wage_per_hour;
+                    System.out.println("Total Hour = " + hour);
+                    System.out.println("Total Wage = " + total_wage + "₹");
+                    System.exit(0);
+                }
                 if (time == 1) {
                     //System.out.println(" Employee is Present Full Day...");
                     month_days++;
                 } else if (time == 0) {
                     //System.out.println(" Employee worked Part Time...");
-                    hour += rd.nextInt(8);
+                    hour += part_time_hour;
                 }
             }
         }
@@ -36,6 +43,6 @@ public class EmployeeWageCal {
         } else if (hour <= max_hour) {
             total_wage = hour * wage_per_hour;
         }
-        System.out.println(name+" Total Wage = " + total_wage + "₹");
+        System.out.println(name + " Total Wage = " + total_wage + "₹");
     }
 }
