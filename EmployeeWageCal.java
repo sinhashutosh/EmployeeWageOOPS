321dc5748f7fb2e73b1aab27ce2ff0e9de74213c
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-26 22:17:25 +0530

    UC7 Completed

--- EmployeeWageCal.java
+++ EmployeeWageCal.java
@@ -7,6 +7,7 @@ public class EmployeeWageCal {
     Random rd = new Random();
     int wage_per_hour = 20;
     int wage_per_day = 160;
+    int part_time_hour = 4;
     int month_days = 0;
     int hour = 0;
     int total_wage = 0;
@@ -14,6 +15,14 @@ public class EmployeeWageCal {
     void wageCalculator() {
         while (month_days < 20 && hour < 100) {
             int attendance = rd.nextInt(2);
+            if (hour == 96) {
+                System.out.println("Now You can do only Part time work");
+                hour += part_time_hour;
+                total_wage = hour * wage_per_hour;
+                System.out.println("Total Hour = " + hour);
+                System.out.println("Total Wage = " + total_wage + "₹");
+                System.exit(0);
+            }
             if (attendance == 1) {
                 int time = rd.nextInt(2);
                 if (time == 1) {
@@ -21,7 +30,7 @@ public class EmployeeWageCal {
                     month_days++;
                 } else if (time == 0) {
                     //System.out.println(" Employee worked Part Time...");
-                    hour += rd.nextInt(8);
+                    hour += part_time_hour;
                 }
             }
         }
