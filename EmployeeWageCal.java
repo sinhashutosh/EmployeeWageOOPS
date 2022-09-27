36d6924214d7b726a1f07d7439873b47664c7bc6
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-27 12:32:45 +0530

    using only if

--- EmployeeWageCal.java
+++ EmployeeWageCal.java
@@ -7,16 +7,14 @@ public class EmployeeWageCal {
     int wage = 0;
 
     void wageCalculator(int isPresent) {
-        switch (isPresent) {
-            case 0:
-                wage = wage_per_hour * isPresent;
-                break;
-            case 1:
-                wage = wage_per_hour * part_time_hour;
-                break;
-            case 2:
-                wage = wage_per_hour * full_day_hour;
-                break;
+        if (isPresent == 0) {
+            wage = wage_per_hour * isPresent;
+        }
+        if (isPresent == 1) {
+            wage = wage_per_hour * part_time_hour;
+        }
+        if (isPresent == 2) {
+            wage = wage_per_hour * full_day_hour;
         }
         System.out.println("Employee Wage = " + wage);
     }
